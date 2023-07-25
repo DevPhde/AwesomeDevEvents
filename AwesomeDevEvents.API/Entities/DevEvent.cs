9a3120e5ceb9f4e967f4560d5f95498a6b966af1
Diego Baumbach <113299561+DevPhde@users.noreply.github.com>
2023-07-25 14:21:26 -0300

    Adicionar arquivos de projeto.

---
+++ AwesomeDevEvents.API/Entities/DevEvent.cs
@@ -0,0 +1,33 @@
+﻿namespace AwesomeDevEvents.API.Entities
+{
+	public class DevEvent
+	{
+        public DevEvent()
+        {
+           Speakers = new List<DevEventSpeaker> { };
+			isDeleted = false;
+        }
+
+        public Guid Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public List<DevEventSpeaker> Speakers { get; set; }
+
+		public bool isDeleted { get; set; }
+
+		public void update(string title, string description, DateTime startDate, DateTime endDate)
+		{
+			title = Title;
+			description = Description;
+			startDate = StartDate;
+			endDate = EndDate;
+		}
+
+		public void delete()
+		{
+			isDeleted = true;
+		}
+	}
+}
